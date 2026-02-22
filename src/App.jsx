334289def7684f7e3b7149704a7b08c9528b55bd
feichulin <feimosqueda777@gmail.com>
2026-02-22 00:58:23 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Header from './components/Header'
+
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <h1 className='mt-12 text-4xl text-neutral-0 font-bold flex justify-center'>Extension List</h1>
+
+    </>
+  )
+}
+
+export default App
